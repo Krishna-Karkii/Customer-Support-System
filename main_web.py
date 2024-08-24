13e7af9c8a524afd345c62d0fd582aba239edb29
krishna <krishnakarki1996@gmail.com>
2024-08-24 19:44:53 +0545

    create basic echo bot which responds to the user prompt

--- main_web.py
+++ main_web.py
@@ -1,1 +1,17 @@
+import streamlit as st
+import time
+
+# Set the title
+st.title("Customer Support System")
+
+
+# ask for user prompt and display the message
+if user_prompt := st.chat_input("Say Something"):
+    with st.chat_message("user"):
+        st.markdown(user_prompt)
+
+    # sleep for 1.5 seconds before responding
+    time.sleep(1.5)
+    with st.chat_message("assistant"):
+        st.markdown(user_prompt)
 
