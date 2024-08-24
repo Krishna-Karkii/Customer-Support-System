81aa6d694781add49bb2c63ffdbc39ea03e144cb
krishna <krishnakarki1996@gmail.com>
2024-08-24 20:00:22 +0545

    create messages list, save and display history of messages

--- main_web.py
+++ main_web.py
@@ -4,14 +4,26 @@ import time
 # Set the title
 st.title("Customer Support System")
 
+# only create messages list if it doesn't exist to save the history of interaction with bot
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
-# ask for user prompt and display the message
+# display history of messages on rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["message"])
+
+# ask for user prompt and display the message, and append it to the messages list
 if user_prompt := st.chat_input("Say Something"):
     with st.chat_message("user"):
         st.markdown(user_prompt)
+
+    st.session_state.messages.append({"role": "user", "message": user_prompt})
 
     # sleep for 1.5 seconds before responding
     time.sleep(1.5)
     with st.chat_message("assistant"):
         st.markdown(user_prompt)
+
+    st.session_state.messages.append({"role": "assistant", "message": user_prompt})
 
